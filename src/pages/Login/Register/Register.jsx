b2f0f4bdf63dfafe7562f17a081565d9c39c70ce
Mustakim <sycospike1@gmail.com>
2023-05-01 15:53:52 +0600

    added reggister input

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Providers/AuthProvider';
 
 const Register = () => {
+    const {createUser} = useContext(AuthContext);
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(name,photo,email,password);
+    }
+
     return (
-        <div>
-            <h2>Register</h2>
-        </div>
+        <Container className='w-25 mx-auto'>
+        <h3>Please Register</h3>
+          <Form onSubmit={handleRegister}>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Name</Form.Label>
+    <Form.Control type="text" name='name' placeholder="Enter your name" required/>
+    
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Photo URL</Form.Label>
+    <Form.Control type="text" name='photo' placeholder="Enter your photo URL" required/>
+    
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" name='email' placeholder="Enter email" required/>
+    
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" name='password' placeholder="Password" required/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" name='accept' label="Accept Terms & Conditions" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Register
+  </Button>
+  <br />
+  <Form.Text className="text-secondary">
+     Already Have an Account ? <Link to='/register'>Login</Link>
+    </Form.Text>
+  <Form.Text className="text-success">
+      
+    </Form.Text>
+  <Form.Text className="text-darger">
+      
+    </Form.Text>
+</Form>
+    </Container>
     );
 };
 
